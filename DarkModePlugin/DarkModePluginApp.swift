a9eb5d2438644ea08329771ad27910a4c07ad0ed
Jaival Patel <pateljaival05@gmail.com>
2022-09-02 22:24:36 -0400

    Initial Commit

---
+++ DarkModePlugin/DarkModePluginApp.swift
@@ -0,0 +1,17 @@
+//
+//  DarkModePluginApp.swift
+//  DarkModePlugin
+//
+//  Created by Jaival Patel on 2022-09-02.
+//
+
+import SwiftUI
+
+@main
+struct DarkModePluginApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
